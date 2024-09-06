db8a117327f8532f43af0dac205a7104b2e59e13
icambraa <126367844+icambraa@users.noreply.github.com>
2024-09-06 02:12:13 +0200

    fixed cors

--- linkito/src/main/java/com/linkito/linkito/config/WebConfig.java
+++ linkito/src/main/java/com/linkito/linkito/config/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Especifica el origen permitido
+                .allowedOrigins(
+                    "http://localhost:5173",      // Origen para desarrollo local
+                    "https://linkito.netlify.app" // Origen para producción en Netlify
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // Permite el uso de cookies y autenticación
